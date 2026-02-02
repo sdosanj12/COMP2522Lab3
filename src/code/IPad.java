bd93b1738999672cbc1acd8e594f23e2712c2725
Ziad Malik <zmalik4@my.bcit.ca>
2026-02-01 23:02:37 -0800

    inital files

---
+++ src/code/IPad.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2522.code;
+
+import java.util.Objects;
+
+/**
+ * Represents an iPad device designed for learning.
+ * IPads are considered equal if they have the same operating system version.
+ *
+ * @author Ziad Malik
+ * @version 1.0
+ */
+public class IPad extends IDevice
+{
+
+}
---
+++ src/code/IPhone17.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2522.code;
+
+import java.util.Objects;
+
+/**
+ * Represents an iPhone 17 device with enhanced features.
+ * iPhone17 objects are considered equal if they have the same minutes remaining
+ * and the same high-resolution camera status.
+ *
+ * @author Ziad Malik
+ * @version 1.0
+ */
+public class IPhone17 extends IPhone
+{
+
+}
