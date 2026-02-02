b5489241ee7548d18e09fa7b3178404188ce3bf1
Ziad Malik <zmalik4@my.bcit.ca>
2026-02-01 23:14:49 -0800

    initial ipad and iphone, need idevice to makesure they matchup

--- src/code/IPad.java
+++ src/code/IPad.java
@@ -11,5 +11,120 @@ import java.util.Objects;
  */
 public class IPad extends IDevice
 {
+    private static final String IPAD_PURPOSE = "learning";
 
+    private final boolean hasCase;
+    private final String osVersion;
+
+    /**
+     * Constructs an IPad with the specified case status and OS version.
+     *
+     * @param hasCase whether the iPad has a case
+     * @param osVersion the operating system version (must not be null or blank)
+     * @throws IllegalArgumentException if osVersion is null or blank
+     */
+    public IPad(final boolean hasCase,
+                final String osVersion)
+    {
+        super(IPAD_PURPOSE);
+        validateOsVersion(osVersion);
+
+        this.hasCase = hasCase;
+        this.osVersion = osVersion;
+    }
+
+    /**
+     * Returns whether this iPad has a case.
+     *
+     * @return true if the iPad has a case, false otherwise
+     */
+    public boolean getHasCase()
+    {
+        return hasCase;
+    }
+
+    /**
+     * Returns the operating system version of this iPad.
+     *
+     * @return the OS version
+     */
+    public String getOsVersion()
+    {
+        return osVersion;
+    }
+
+    /**
+     * Prints all details of this iPad.
+     */
+    @Override
+    public void printDetails()
+    {
+        System.out.println(toString());
+    }
+
+    /**
+     * Returns a string representation of this iPad.
+     *
+     * @return a string containing all iPad details
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("%s, HasCase=%b, OsVersion=%s",
+                super.toString(),
+                hasCase,
+                osVersion);
+    }
+
+    /**
+     * Determines if this iPad is equal to another object.
+     * Two iPads are equal if they have the same operating system version.
+     *
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
+    @Override
+    public boolean equals(final Object obj)
+    {
+        final IPad other;
+
+        if (this == obj)
+        {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+
+        other = (IPad) obj;
+
+        return Objects.equals(osVersion, other.osVersion);
+    }
+
+    /**
+     * Returns a hash code value for this iPad.
+     *
+     * @return a hash code value
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(osVersion);
+    }
+
+    /**
+     * Validates that the OS version is not null or blank.
+     *
+     * @param osVersion the OS version to validate
+     * @throws IllegalArgumentException if osVersion is null or blank
+     */
+    private static void validateOsVersion(final String osVersion)
+    {
+        if (osVersion == null || osVersion.isBlank())
+        {
+            throw new IllegalArgumentException("OS version must not be null or blank.");
+        }
+    }
 }
--- src/code/IPhone.java
+++ src/code/IPhone.java
@@ -11,5 +11,136 @@ import java.util.Objects;
  */
 public class IPhone extends IDevice
 {
+    private static final String IPHONE_PURPOSE = "talking";
+    private static final double MIN_MINUTES = 0.0;
 
+    private final double minutesRemaining;
+    private final String carrier;
+
+    /**
+     * Constructs an IPhone with the specified minutes remaining and carrier.
+     *
+     * @param minutesRemaining the minutes remaining on the phone plan (must not be negative)
+     * @param carrier the phone carrier (must not be null or blank)
+     * @throws IllegalArgumentException if minutesRemaining is negative or carrier is null or blank
+     */
+    public IPhone(final double minutesRemaining,
+                  final String carrier)
+    {
+        super(IPHONE_PURPOSE);
+        validateMinutesRemaining(minutesRemaining);
+        validateCarrier(carrier);
+
+        this.minutesRemaining = minutesRemaining;
+        this.carrier = carrier;
+    }
+
+    /**
+     * Returns the minutes remaining on this iPhone's phone plan.
+     *
+     * @return the minutes remaining
+     */
+    public double getMinutesRemaining()
+    {
+        return minutesRemaining;
+    }
+
+    /**
+     * Returns the carrier for this iPhone.
+     *
+     * @return the carrier
+     */
+    public String getCarrier()
+    {
+        return carrier;
+    }
+
+    /**
+     * Prints all details of this iPhone.
+     */
+    @Override
+    public void printDetails()
+    {
+        System.out.println(toString());
+    }
+
+    /**
+     * Returns a string representation of this iPhone.
+     *
+     * @return a string containing all iPhone details
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("%s, MinutesRemaining=%.1f, Carrier=%s",
+                super.toString(),
+                minutesRemaining,
+                carrier);
+    }
+
+    /**
+     * Determines if this iPhone is equal to another object.
+     * Two iPhones are equal if they have the same amount of minutes remaining.
+     *
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
+    @Override
+    public boolean equals(final Object obj)
+    {
+        final IPhone other;
+
+        if (this == obj)
+        {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+
+        other = (IPhone) obj;
+
+        return Double.compare(minutesRemaining, other.minutesRemaining) == 0;
+    }
+
+    /**
+     * Returns a hash code value for this iPhone.
+     *
+     * @return a hash code value
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(minutesRemaining);
+    }
+
+    /**
+     * Validates that the minutes remaining is not negative.
+     *
+     * @param minutesRemaining the minutes remaining to validate
+     * @throws IllegalArgumentException if minutesRemaining is negative
+     */
+    private static void validateMinutesRemaining(final double minutesRemaining)
+    {
+        if (minutesRemaining < MIN_MINUTES)
+        {
+            throw new IllegalArgumentException("Minutes remaining cannot be negative: " + minutesRemaining);
+        }
+    }
+
+    /**
+     * Validates that the carrier is not null or blank.
+     *
+     * @param carrier the carrier to validate
+     * @throws IllegalArgumentException if carrier is null or blank
+     */
+    private static void validateCarrier(final String carrier)
+    {
+        if (carrier == null || carrier.isBlank())
+        {
+            throw new IllegalArgumentException("Carrier must not be null or blank.");
+        }
+    }
 }
